5948e726c553857d836d7323cb2f4c8bf490de09
ptvu310303@gmail.com <163112690+PhamTuanVu31032003@users.noreply.github.com>
2024-10-18 00:51:59 +0700

    Add files via upload

---
+++ lab8/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> firstlists = new List<int>();
+        //Console.WriteLine("do dai cua mang la {0}",firstlist.Count);
+        firstlists.Add(1); // chi so 0, phan tu 1
+        firstlists.Add(2);
+        firstlists.Add(3);
+        firstlists.Add(5);
+        //Console.WriteLine("do dai cua mang la {0}", firstlist.Count);
+        //Console.WriteLine("capacity{0}", firstlist.Capacity);
+        firstlists.Add(4);
+        firstlists.Add(7);
+        //Console.WriteLine("do dai mang la{0}", firstlist.Count); 
+        //Console.WriteLine("capacity{0}", firstlist.Capacity);
+        firstlists.Add(6);
+        firstlists.Add(9);
+       // Console.WriteLine("do dai mang la{0}", firstlist.Count);
+        //Console.WriteLine("capacity{0}", firstlist.Capacity);
+         //Console.WriteLine(firstlist.Contains(8));
+         /*firstlists.RemoveAt(5); // remove chi so
+         foreach (var item in firstlists){
+            Console.WriteLine(item);
+         } */
+         firstlists.Sort();
+         foreach (var item in firstlists){
+            Console.WriteLine(item);
+         }
+       
+        /* firstlists.Reverse();
+         foreach (var item in firstlists){
+            Console.WriteLine(item);
+         }*/
+         thuong ngu
+
+    }
+}
